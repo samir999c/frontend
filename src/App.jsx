b87b23d851870533462a0e1e8e37c81ef98f4bb7
samir999c <samirchudal00@gmail.com>
2025-10-30 19:27:43 +1100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import KoalaRoute from "./components/KoalaRoute";
 // --- NEW IMPORTS for Amadeus Booking Flow ---
 import FlightResultsPage from "./components/FlightResultsPage";
 import PassengerFormPage from "./components/PassengerFormPage";
-import PaymentConfirmationPage from "./components/PaymentConfirmationPage";
 import BookingConfirmationPage from "./components/BookingConfirmationPage";
 // ---------------------------------------------
 
@@ -22,7 +21,6 @@ export default function App() {
         {/* --- NEW AMADEUS BOOKING FLOW ROUTES --- */}
         <Route path="/flights/results" element={<FlightResultsPage />} />
         <Route path="/flights/passengers" element={<PassengerFormPage />} />
-        <Route path="/flights/payment" element={<PaymentConfirmationPage />} />
         <Route path="/flights/confirm/:orderId" element={<BookingConfirmationPage />} />
         {/* -------------------------------------- */}
 
