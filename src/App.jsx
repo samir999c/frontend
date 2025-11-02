dc162130fe3561da39b2ceeecf229fc652150af2
samir999c <samirchudal00@gmail.com>
2025-11-02 19:17:44 +1100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-// src/App.jsx
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-// --- THIS IS THE MISSING LINE ---
-import Layout from "./components/Layout"; 
-// ---------------------------------
-
+import Layout from "./components/Layout";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
@@ -16,31 +10,31 @@ import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
 import KoalaRoute from "./components/KoalaRoute";
 
-// --- NEW IMPORTS for Amadeus Booking Flow ---
 import FlightResultsPage from "./components/FlightResultsPage";
 import PassengerFormPage from "./components/PassengerFormPage";
 import BookingConfirmationPage from "./components/BookingConfirmationPage";
-// ---------------------------------------------
+
+// --- NEW IMPORT ---
+import FlightDetailsPage from "./components/FlightDetailsPage"; 
+// ------------------
 
 export default function App() {
   return (
     <Routes>
-      {/* This line was causing the error because 'Layout' was not imported */}
       <Route path="/" element={<Layout />}>
-        
+        {/* --- Your Existing Routes --- */}
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* ... other routes ... */}
         <Route path="/koalaroute" element={<KoalaRoute />} />
         
-        {/* --- NEW AMADEUS BOOKING FLOW ROUTES --- */}
+        {/* --- UPDATED AMADEUS FLOW --- */}
         <Route path="/flights/results" element={<FlightResultsPage />} />
+        <Route path="/flights/details" element={<FlightDetailsPage />} /> {/* <-- NEW ROUTE */}
         <Route path="/flights/passengers" element={<PassengerFormPage />} />
         <Route path="/flights/confirm/:orderId" element={<BookingConfirmationPage />} />
-        {/* -------------------------------------- */}
+        {/* --------------------------- */}
 
         <Route path="*" element={<NotFound />} />
       </Route>
